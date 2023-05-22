b9febd1f08a5f766f476e90e0b3ba408865395df
CodingNewbie0 <ahrgjdy@gmail.com>
2023-05-22 17:32:14 +0900

    5월 22일

---
+++ Day04/Day04_Wab/aspnet01_wepapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnet01_wepapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
