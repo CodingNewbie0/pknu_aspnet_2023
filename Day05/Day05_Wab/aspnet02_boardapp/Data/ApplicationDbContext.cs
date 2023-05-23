2f1c8b8b40cb54b11f99b6bea047eedb92388a85
CodingNewbie0 <ahrgjdy@gmail.com>
2023-05-23 17:25:01 +0900

    5월 23일

---
+++ Day05/Day05_Wab/aspnet02_boardapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using aspnet02_boardapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnet02_boardapp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions option) : base(option) 
+        {
+        }
+        public DbSet<Board> Boards { get; set; }
+    }
+}
